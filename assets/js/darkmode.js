bf8f6fa481623f4611ae2dbe7af13baa98ff744e
João Vitor da Costa Andrade <joaovcostaandrade@gmail.com>
2024-05-09 16:55:51 -0300

    defaultDark

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,6 +1,6 @@
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
+//var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+//var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+localStorage.setItem('color-theme', 'dark')
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     themeToggleLightIcon.classList.remove('hidden');
